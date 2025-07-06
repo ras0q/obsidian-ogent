8877b9f2979f0fbd9461d9064b7271e54d2fe2a4
ras0q <ras0q@users.noreply.github.com>
2025-07-07 00:18:45 +0900

    remove weather agent

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,11 +1,9 @@
 import { Mastra } from "@mastra/core/mastra";
 import { PinoLogger } from "@mastra/loggers";
-import { weatherWorkflow } from "./workflows/weather-workflow.ts";
 import { Agent } from "@mastra/core";
 
 export const buildMastra = (agents: { [key: string]: Agent }) =>
   new Mastra({
-    workflows: { weatherWorkflow },
     agents,
     logger: new PinoLogger({
       name: "Mastra",
